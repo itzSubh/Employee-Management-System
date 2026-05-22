96d576a605080ff3933b1be2c956a4fec7242717
itzSubh <subhash03id@gmail.com>
2026-05-22 10:20:32 +0530

    feat: implement routing and add main pages for the employee management system

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-
+import {Toaster} from 'react-hot-toast'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard.jsx'
+import Employees from './pages/Employees.jsx'
+import Attendance from './pages/Attendance.jsx'
+import Leave from './pages/Leave.jsx'
+import Payslips from './pages/Payslips.jsx'
+import LoginLanding from './pages/LoginLanding.jsx'
+import Settings from './pages/Settings.jsx'
+import Layout from './pages/Layout.jsx'
+import { Navigate } from 'react-router-dom'
+import PrintPayslip from './pages/PrintPayslip.jsx'
 const App = () => {
   return (
-    <div>
-      Hii
-    </div>
+    <>
+    <Toaster />
+    <Routes>
+      <Route path='/login' element={ <LoginLanding/> } />
+      <Route element= {<Layout />}>
+          <Route path='/dashboard' element= {<Dashboard />}/>
+          <Route path='/employees' element= {<Employees />}/>
+          <Route path='/attendance' element= {<Attendance />}/>
+          <Route path='/leave' element= {<Leave />}/>
+          <Route path='/payslips' element= {<Payslips />}/>
+          <Route path='/settings' element= {<Settings />}/> 
+      </Route>
+      <Route path='/print/payslips/:id' element = { <PrintPayslip/>}/>
+      <Route path='*' element = {<Navigate to='/dashboard' replace />}/>
+    </Routes>
+    </>
   )
 }
 
---
+++ EMS/src/pages/PrintPayslip.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PrintPayslip = () => {
+  return (
+    <div>
+      PrintPaySlip
+    </div>
+  )
+}
+
+export default PrintPayslip
